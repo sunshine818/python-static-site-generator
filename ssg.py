901520d8079bcc201d9d814be371117a4fd62286
sunshine818 <49012524+sunshine818@users.noreply.github.com>
2020-11-11 12:05:10 -0500

    Finding the error

--- ssg.py
+++ ssg.py
@@ -1,1 +1,1 @@
-import typerfrom ssg.site import Sitedef main(source="content", dest="dist"):    config = {            "source": source,             "dest": dest            }    Site(**config).build()    typer.run(main)
+import typerfrom ssg.site import Sitedef main(source="content", dest="dist"):    config = {"source": source, "dest": dest}    Site(**config).build()typer.run(main)
