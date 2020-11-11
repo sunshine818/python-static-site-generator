6466fef6d290c9ad6c27257aa4ab8cbc30ce007d
sunshine818 <49012524+sunshine818@users.noreply.github.com>
2020-11-11 11:30:45 -0500

    Site Building
    Built the Site class

---
+++ ssg.py
@@ -0,0 +1,1 @@
+import typerimport ssg.sitedef main(source="content", dest="dist"):    config = {            "source": source,             "dest": dest            }    built_site = ssg.site.Site(**config)    built_site.build()    typer.run(main())
