9c7a4d844b3dc7952f4382e4481f7f8a6f5c6d71
sunshine818 <49012524+sunshine818@users.noreply.github.com>
2020-11-11 15:38:20 -0500

    Lots of updates

--- ssg.py
+++ ssg.py
@@ -1,1 +1,1 @@
-import typerfrom ssg.site import Siteimport ssg.parsersdef main(source="content", dest="dist"):    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}    Site(**config).build()typer.run(main)
+import typerfrom ssg.site import Siteimport ssg.parsersdef main(source="content", dest="dist"):    config = {"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]}    Site(**config).build()typer.run(main)
